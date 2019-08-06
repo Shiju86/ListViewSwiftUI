b778c933834699bc785fed56c137ba47ce8e3e40
Shiju Varghese <shiju.varghese@exilant.com>
2019-08-06 14:15:28 +0530

    Initial Commit

---
+++ ListSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ListSwiftUI
+//
+//  Created by Shiju Varghese on 06/08/19.
+//  Copyright © 2019 Shiju Varghese. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
